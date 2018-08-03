5dde71c826f7fb4b65336f1d95b019868138bce4
weihua.hwh <weihua.hwh@alibaba-inc.com>
2018-08-03 20:57:15 +0800

    first commit

---
+++ rainbow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rainbow
+//
+//  Created by huanwh on 2018/8/3.
+//  Copyright © 2018年 hwh. All rights reserved.
+//
+
+import UIKit
+import NEKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
